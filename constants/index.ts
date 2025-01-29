82050415e19b34b2d8e4be10164bb00c432e18d5
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-01-29 18:49:41 +0530

    Added onboarding screen 1

--- constants/index.ts
+++ constants/index.ts
@@ -27,7 +27,7 @@ import check from "@/assets/images/check.png";
 import getStarted from "@/assets/images/get-started.png";
 import message from "@/assets/images/message.png";
 import noResult from "@/assets/images/no-result.png";
-import onboarding1 from "@/assets/images/onboarding1.png";
+import onboarding1 from "@/assets/images/onboarding1.jpeg";
 import onboarding2 from "@/assets/images/onboarding2.png";
 import onboarding3 from "@/assets/images/onboarding3.png";
 import signUpCar from "@/assets/images/signup-car.png";
