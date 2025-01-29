d36afea2ca544f9ecfc8640e80edfa180fbc20d0
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-01-29 18:57:04 +0530

    Chnage the onboarding images

--- constants/index.ts
+++ constants/index.ts
@@ -28,8 +28,8 @@ import getStarted from "@/assets/images/get-started.png";
 import message from "@/assets/images/message.png";
 import noResult from "@/assets/images/no-result.png";
 import onboarding1 from "@/assets/images/onboarding1.jpeg";
-import onboarding2 from "@/assets/images/onboarding2.png";
-import onboarding3 from "@/assets/images/onboarding3.png";
+import onboarding2 from "@/assets/images/onboarding2.jpeg";
+import onboarding3 from "@/assets/images/onboarding3.jpeg";
 import signUpCar from "@/assets/images/signup-car.png";
 
 export const images = {
